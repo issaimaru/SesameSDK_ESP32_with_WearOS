700f8c90b753288668ee6131566dfeb6dd9b7d1a
issaimaru <80198387+issaimaru@users.noreply.github.com>
2025-05-02 22:11:54 +0900

    first commit

---
+++ main/main.c
@@ -0,0 +1,19 @@
+#include "blecent.h"
+#include "nvs_flash.h"
+#include "ssm_cmd.h"
+
+static const char * TAG = "main.c";
+
+static void ssm_action_handle(sesame * ssm) {
+    ESP_LOGI(TAG, "[ssm_action_handle][ssm status: %s]", SSM_STATUS_STR(ssm->device_status));
+    if (ssm->device_status == SSM_UNLOCKED) {
+        ssm_lock(NULL, 0);
+    }
+}
+
+void app_main(void) {
+    ESP_LOGI(TAG, "SesameSDK_ESP32 [11/24][087]");
+    nvs_flash_init();
+    ssm_init(ssm_action_handle);
+    esp_ble_init();
+}
